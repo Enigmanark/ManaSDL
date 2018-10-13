c51aa53f941b88c09b1dc067bbc5a84f6c1ed623
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-10-13 12:40:54 -0500

    More work on TextureManager

--- Mana/TextureManager.h
+++ Mana/TextureManager.h
@@ -5,14 +5,28 @@
 #ifndef __TextureManager__
 #define __TextureManager__
 
+typedef TextureManager TheTextureManager;
+
 class TextureManager {
 
 public:
+
+	static TextureManager* Instance() {
+		if (s_Instance == 0) {
+			s_Instance = new TextureManager();
+			return s_Instance;
+		}
+		else return s_Instance;
+	}
 
 	bool Load(std::string fileName, std::string id, SDL_Renderer* pRenderer);
 	void Draw(std::string id, int x, int y, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
 private:
+	TextureManager() {}
+	~TextureManager() {}
+
+	static TextureManager* s_Instance;
 	std::map<std::string, SDL_Texture*> m_textureMap;
 };
 
