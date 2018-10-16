3c051aea3d7c58c1cca48cd5b70a3157cd12e792
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-10-16 17:20:32 -0500

    Restructured and rendering is now successful

--- Mana/GameObject.cpp
+++ Mana/GameObject.cpp
@@ -1,29 +1,69 @@
 #include "GameObject.h"
-
-GameObject::GameObject(std::string p_id) {
-	m_id = p_id;
-}
-
-std::string GameObject::GetId() {
-	return m_id;
-}
-
-void GameObject::Draw() {
-
-}
-
-void GameObject::Update() {
-
-}
-
-void GameObject::Load(std::string p_textureId) {
-	m_textureId = p_textureId;
-}
+#include "TextureManager.h"
+#include "Game.h"
 
 Vector2D* GameObject::GetPosition() {
 	return m_position;
 }
 
+GameObject::~GameObject() {
+	Clean();
+}
+
+GameObject::GameObject(std::string p_id) {
+	m_id = p_id;
+	m_position = new Vector2D(0, 0);
+}
+
+void GameObject::Load(std::string p_filePath, std::string p_textureId) {
+	m_textureId = p_textureId;
+	TheTextureManager::Instance()->Load(p_filePath, p_textureId, TheGame::Instance()->GetRenderer());
+}
+
+void GameObject::LoadSpriteSheet(std::string p_filePath, std::string p_textureId, int p_xFrames, int p_yFrames, int p_startFrameX, int p_startFrameY) {
+	m_textureId = p_textureId;
+	TheTextureManager::Instance()->Load(p_filePath, p_textureId, TheGame::Instance()->GetRenderer());
+	Vector2D size = TheTextureManager::Instance()->GetTextureSize(p_textureId);
+	m_frameHeight = size.GetX() / p_xFrames;
+	m_frameWidth = size.GetY() / p_yFrames;
+	m_currentFrameX = p_startFrameX;
+	m_currentFrameY = p_startFrameY;
+}
+
+void GameObject::Update() {
+	//Todo
+}
+
 void GameObject::Clean() {
+	TheTextureManager::Instance()->ClearFromMap(m_textureId);
 	delete m_position;
+	delete m_bounds;
+}
+
+void GameObject::Draw() {
+	TheTextureManager::Instance()->Draw(m_id, GetPosition()->GetX(), GetPosition()->GetY());
+}
+
+void GameObject::DrawRegion() {
+	int t_srcX = m_frameWidth * m_currentFrameX;
+	int t_srcY = m_frameHeight * m_currentFrameY;
+	int t_destX = m_position->GetX();
+	int t_destY = m_position->GetY();
+	TheTextureManager::Instance()->DrawRegion(m_textureId, t_srcX, t_srcY, m_frameWidth, m_frameHeight, t_destX, t_destY);
+}
+
+void GameObject::SetBounds(int p_x, int p_y, int p_width, int p_height) {
+	m_bounds = new SDL_Rect();
+	m_bounds->x = p_x;
+	m_bounds->y = p_y;
+	m_bounds->w = p_width;
+	m_bounds->h = p_height;
+}
+
+SDL_Rect* GameObject::GetBounds() {
+	return m_bounds;
+}
+
+bool GameObject::DoBoundsCollide(SDL_Rect* p_rect) {
+	return SDL_HasIntersection(m_bounds, p_rect);
 }
--- Mana/GameObject.h
+++ Mana/GameObject.h
@@ -10,20 +10,29 @@ class GameObject {
 public:
 	GameObject() {};
 	GameObject(std::string p_id);
-	~GameObject() { Clean(); }
+	~GameObject();
 
 	std::string GetId();
-	void Load(std::string p_textureId);
+	void Load(std::string p_filePath, std::string p_textureId);
+	void LoadSpriteSheet(std::string p_filePath, std::string p_textureId, int p_xFrames, int p_yFrames, int p_startFrameX, int p_startFrameY);
 	virtual void Update();
 	virtual void Draw();
+	virtual void DrawRegion();
 	virtual void Clean();
 	Vector2D* GetPosition();
+	void SetBounds(int p_x, int p_y, int p_width, int p_height);
+	SDL_Rect* GetBounds();
+	bool DoBoundsCollide(SDL_Rect* p_rect);
 
 protected:
 	std::string m_id;
 	std::string m_textureId;
 	Vector2D* m_position;
-	
+	SDL_Rect* m_bounds;
+	int m_currentFrameX;
+	int m_currentFrameY;
+	int m_frameWidth;
+	int m_frameHeight;
 };
 #endif // !__GameObject__
 
