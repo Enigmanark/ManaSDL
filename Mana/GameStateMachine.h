571b9982694b03a72d839e58254e246e0643a497
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-10-16 00:13:17 -0500

    GameStateMachine written

---
+++ Mana/GameStateMachine.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "GameState.h"
+#include <vector>
+#ifndef __GameStateMachine__
+#define __GameStateMachine__
+
+class GameStateMachine {
+	
+public:
+
+	void PushState(GameState* p_state);
+	void ChangeState(GameState* p_state);
+	void PopState();
+
+private:
+
+	std::vector<GameState*> m_gameStates;
+
+};
+
+#endif // !__GameStateMachine__
